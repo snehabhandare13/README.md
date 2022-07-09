0fefd79a3f4f7e0f07983c09d5a5090d20e27c72
snehabhandare13 <108966975+snehabhandare13@users.noreply.github.com>
2022-07-09 17:41:02 +0530

    Create git .php

---
+++ git .php
@@ -0,0 +1,21 @@
+<?php
+ // 1. download the repository
+ // git clone https://github.com/dpkphp/training.git
+
+ // step 2
+ // steps to commit and push the files to git repository
+ // git add .
+
+ // step 3
+// git commit -m "type your message"
+
+// step 4
+// git push
+
+// see the changes are reflected in your repository
+
+
+// git status (check if any files or folders are changed)
+
+
+// git pull
